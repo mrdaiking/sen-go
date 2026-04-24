351721f0d8e105e460a051aa338dbd6f83256a01
Cuong <cuong@blayn.co.jp>
2026-04-24 10:59:33 +0900

    basic types

--- hello.go
+++ hello.go
@@ -2,42 +2,64 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"math/rand"
-	"math"
+	// "time"
+	// "math/rand"
+	// "math"
+	"math/cmplx"
 )
 
-func add(x int, y int) int {
-	return x + y
-}
+// var c, python, java bool // Define boolean
+// var i, j int = 1, 2 // Variables with initializers
 
-func add1(x,y int) int {
-	return x + y
-}
+// func add(x int, y int) int {
+// 	return x + y
+// }
 
-func swap(x, y string) (string, string) {
-	return y, x
-}
+// func add1(x,y int) int {
+// 	return x + y
+// }
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
+
+// func split(sum int) (x, y int) {
+// 	x = sum * 4 / 9
+// 	y = sum - x
+// 	return
+// }
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+// func main() {
+// 	var i, j int = 1, 2
+// 	k := 3
+	
+// 	fmt.Println("Hello, World")
+// 	fmt.Println("This time is", time.Now())
+// 	fmt.Println("My favorite number is", rand.Intn(10))
+
+// 	fmt.Printf("Now you have %g problem.\n", math.Sqrt(7)) // Use Printf
+
+// 	fmt.Println(math.Pi)
+
+// 	fmt.Println(add(42, 13))
+// 	fmt.Println(add1(42, 13))
+
+// 	a, b := swap("hello", "world")
+// 	fmt.Println(a, b)
+// 	fmt.Println(split(17))
+// 	var c, python, java = true, false, "no!"
+// 	fmt.Println(i, j, k, c, python, java)
+
+// }
 
 func main() {
-	fmt.Println("Hello, World")
-	fmt.Println("This time is", time.Now())
-	fmt.Println("My favorite number is", rand.Intn(10))
-
-	fmt.Printf("Now you have %g problem.\n", math.Sqrt(7)) // Use Printf
-
-	fmt.Println(math.Pi)
-
-	fmt.Println(add(42, 13))
-	fmt.Println(add1(42, 13))
-
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
-	fmt.Println(split(17))
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
